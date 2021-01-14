3fbf1326e0cef70c65e4af015a8ce5bde4945cac
Shay Bergmann <shay.bergmann@iohk.io>
2021-01-14 18:31:25 -0500

    use updated flake utils

--- flake.nix
+++ flake.nix
@@ -1,21 +1,21 @@
 {
   description = "Web interface for Mantis Faucet";
 
-  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.utils.url = "github:kreisys/flake-utils";
 
-  outputs = { self, nixpkgs, flake-utils }:
-    let
-      name = "mantis-faucet-web";
-      systems = [ "x86_64-linux" "x86_64-darwin" ];
-      overlay = final: prev: {
-        ${name}.defaultPackage = final.callPackage ./package.nix {};
-      };
+  outputs = { self, nixpkgs, utils }:
+    utils.lib.simpleFlake
+      {
+        inherit nixpkgs;
+        name = "mantis-faucet-web";
+        systems = [ "x86_64-linux" "x86_64-darwin" ];
+        overlay = final: prev: {
+          mantis-faucet-web = final.callPackage ./package.nix { };
+        };
 
-      simpleFlake = flake-utils.lib.simpleFlake {
-        inherit name systems overlay self nixpkgs;
+        packages = { mantis-faucet-web }: {
+          inherit mantis-faucet-web;
+          defaultPackage = mantis-faucet-web;
+        };
       };
-    in simpleFlake // {
-      hydraJobs = self.packages;
-      inherit overlay;
-    };
 }
